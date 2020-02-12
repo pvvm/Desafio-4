2e71da98560ffe1eaf2fa6b270e00bd48b8ca6c6
Pedro Vitor <pv.mizuno@hotmail.com>
2020-02-12 10:08:13 -0300

    Desafio 4-1

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const routes = require("./routes")
+
+const server = express()
+//const pages = require("./data")
+
+server.use('/static', express.static(__dirname + "/public"))
+server.use(routes)
+//server.use(express.static('public'))
+
+server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+    express:server,
+    autoescape: false,
+    noCache: true
+})
+
+server.listen(5000, function(){
+    console.log("Server is running")
+})
+
+server.use(function(req, res) {
+    res.status(404).render("not-found");
+});
